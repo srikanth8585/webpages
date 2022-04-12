b3195b406573590032a986ad05042aba98066d96
achyutuni srikanth <achyutunisrikanth96@gmail.com>
2022-04-12 17:34:27 +0530

    commit

--- weatherreport.py
+++ weatherreport.py
@@ -3,5 +3,5 @@ from pprint import pprint
 API_Key = '744c85a34fe812c60c24c9696ef9b5ca'
 city = input("Enter a city:")
 base_url = "http://api.openweathermap.org/data/2.5/weather?appid="+API_Key+"&q="+city
-weather_data = requests.get(base_url)
+weather_data = requests.get(base_url).json
 pprint(weather_data)
