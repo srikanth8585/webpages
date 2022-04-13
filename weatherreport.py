ecfd9128bc8e8f9fa9ddfff75098ee70f4a2d88d
achyutuni srikanth <achyutunisrikanth96@gmail.com>
2022-04-13 17:43:30 +0530

    update the files

--- weatherreport.py
+++ weatherreport.py
@@ -1,8 +1,7 @@
 import requests
-import json
 from pprint import pprint
 API_Key = '744c85a34fe812c60c24c9696ef9b5ca'
 city = input("Enter a city:")
-base_url = "http://api.openweathermap.org/data/2.5/weather?appid="+API_Key+"&q="+city
-weather_data = requests.get(base_url).json
+base_url = "https://api.openweathermap.org/data/2.5/weather?appid="+API_Key+"&q="+city
+weather_data = requests.get(base_url).json()
 pprint(weather_data)
